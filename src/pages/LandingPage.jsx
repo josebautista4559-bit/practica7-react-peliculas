efb9eef458e7f818fef2da9927f7ae9cdb3f42d6
josebautista4559-bit <jose.bautista4559@alumnos.udg.mx>
2026-04-08 16:36:10 -0600

    feat: configuracion de router principal

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+/* Prueba para verificar la funcionalidad de AppRouter */
+/* integrante 2 debe modificar */
+
+export default function LandingPage() {
+    return;
+}
+
+
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,6 @@
+/* Prueba para verificar la funcionalidad de AppRouter */
+/* integrante 2 debe modificar */
+
+export default function MovieDetails() {
+    return;
+}
